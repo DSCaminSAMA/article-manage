73683f5243cc16aab8e3bd688f66884a007a1c5e
testouya <1422749473@qq.com>
2019-06-03 22:03:06 +0800

    添加实体类

--- articlemanagement/src/main/java/com/liu/article/mapper/AdminMapper.java
+++ articlemanagement/src/main/java/com/liu/article/mapper/AdminMapper.java
@@ -1,4 +1,13 @@
 package com.liu.article.mapper;
 
+import com.liu.article.pojo.Admin;
+
 public interface AdminMapper {
+
+    Admin login(String a_name);
+
+    void insert(Admin admin);
+
+    Admin findByName(String a_name);
+
 }
--- articlemanagement/src/main/java/com/liu/article/pojo/Reply.java
+++ articlemanagement/src/main/java/com/liu/article/pojo/Reply.java
@@ -1,4 +1,94 @@
 package com.liu.article.pojo;
 
-public class Reply {
+import java.io.Serializable;
+
+public class Reply implements Serializable {
+
+    //回复信息编号
+    private int lr_id;
+
+    //回复人
+    private String lr_name;
+
+    //回复日期
+    private String lr_date;
+
+    //回复内容
+    private String lr_content;
+
+    //给谁留言
+    private String lr_for_name;
+
+    //哪条留言的回复
+    private String lr_for_word;
+
+    //给哪条回复信息回复的
+    private String lr_for_reply;
+
+    //在哪条文章下的回复
+    private String lr_for_article_id;
+
+    public int getLr_id() {
+        return lr_id;
+    }
+
+    public void setLr_id(int lr_id) {
+        this.lr_id = lr_id;
+    }
+
+    public String getLr_name() {
+        return lr_name;
+    }
+
+    public void setLr_name(String lr_name) {
+        this.lr_name = lr_name;
+    }
+
+    public String getLr_date() {
+        return lr_date;
+    }
+
+    public void setLr_date(String lr_date) {
+        this.lr_date = lr_date;
+    }
+
+    public String getLr_content() {
+        return lr_content;
+    }
+
+    public void setLr_content(String lr_content) {
+        this.lr_content = lr_content;
+    }
+
+    public String getLr_for_name() {
+        return lr_for_name;
+    }
+
+    public void setLr_for_name(String lr_for_name) {
+        this.lr_for_name = lr_for_name;
+    }
+
+    public String getLr_for_word() {
+        return lr_for_word;
+    }
+
+    public void setLr_for_word(String lr_for_word) {
+        this.lr_for_word = lr_for_word;
+    }
+
+    public String getLr_for_reply() {
+        return lr_for_reply;
+    }
+
+    public void setLr_for_reply(String lr_for_reply) {
+        this.lr_for_reply = lr_for_reply;
+    }
+
+    public String getLr_for_article_id() {
+        return lr_for_article_id;
+    }
+
+    public void setLr_for_article_id(String lr_for_article_id) {
+        this.lr_for_article_id = lr_for_article_id;
+    }
 }
