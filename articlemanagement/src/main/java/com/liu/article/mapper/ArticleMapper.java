da300ab6c3e1c98a2eee001fe885fea95b252306
[暧昧的大叔叔] <[1010515662@qq.com]>
2019-06-05 14:30:31 +0800

    mapper

--- articlemanagement/src/main/java/com/liu/article/mapper/ArticleMapper.java
+++ articlemanagement/src/main/java/com/liu/article/mapper/ArticleMapper.java
@@ -1,4 +1,40 @@
 package com.liu.article.mapper;
 
+import com.liu.article.pojo.Article;
+import com.liu.article.pojo.Reply;
+import com.liu.article.pojo.Words;
+
+import java.util.HashMap;
+import java.util.List;
+
 public interface ArticleMapper {
+
+    void saveArticle(Article article);
+
+    int selectCount();
+
+    List<Article> findByPage(HashMap<String,Object> map);
+
+    List<Article> findConByPage(HashMap<String,Object> conMap);
+
+    void delete(int r_id);
+
+    void update(Article article);
+
+    Article findById(int r_id);
+
+    void clean(int r_id);
+
+    int selectCount2();
+
+    void restore(int r_id);
+
+    void saveWords(Words words);
+
+    void saveReply(Reply reply);
+
+    List<Words> findByWords();
+
+    List<Reply> findByReply();
+
 }
